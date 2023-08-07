91a829f76ae247760b440ffe6c41856af429a9e3
Sil <silkreulen@gmail.com>
2023-08-07 16:35:16 +0200

    feat: include storybook

--- apps/next_storybook/.storybook/preview.tsx
+++ apps/next_storybook/.storybook/preview.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import type { Preview } from "@storybook/react";
 import "../global.css";
+import Head from "next/head";
 import { fontSans } from "ui/lib/fonts";
 import { cn } from "ui/lib/utils";
 
@@ -21,15 +22,28 @@ const preview: Preview = {
   decorators: [
     (Story) => {
       return (
-        <div
+        <main
           className={cn(
-            "bg-background font-sans antialiased",
+            "min-h-screen bg-slate-500 font-sans antialiased",
             fontSans.variable
           )}
         >
+          <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossOrigin=""
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+              rel="stylesheet"
+            />
+          </Head>
+          {/* eslint-disable-next-line */}
           {/* @ts-ignore */}
           <Story />
-        </div>
+        </main>
       );
     },
   ],
--- apps/next_storybook/tailwind.config.js
+++ apps/next_storybook/tailwind.config.js
@@ -2,6 +2,7 @@ const sharedConfig = require("tailwind-config/tailwind.config.js");
 
 module.exports = {
   presets: [sharedConfig],
+
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
