461ec107a64982c6f0f1eb52b7e7af24e1dec0f2
Sil <silkreulen@gmail.com>
2023-10-09 15:24:13 +0200

    fix: test commitlint

--- apps/web/src/app/components/ThemeToggle.tsx
+++ apps/web/src/app/components/ThemeToggle.tsx
@@ -13,7 +13,7 @@ export const ThemeToggle = () => {
       size="sm"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <SunMedium className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      {/* <SunMedium className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" /> */}
       <Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
     </Button>
