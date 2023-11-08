7ad4b0f5d7a4b75cd85c5ba1df51cade425546be
Sil <silkreulen@gmail.com>
2023-11-08 14:24:17 +0100

    feat: remove storybook

--- apps/storybook/config/site.ts
+++
@@ -1,9 +0,0 @@
-export type SiteConfig = typeof siteConfig;
-
-export const siteConfig = {
-  name: "Peakfijn",
-  description: "The scalable monorepo and full-stack starter pack.",
-  url: "turbo-starter-web.vercel.app",
-  mainNav: [],
-  links: {},
-};
